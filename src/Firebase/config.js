bfdc6c261d52af950306ba0863b5427c0ad4613f
Asda <omega-faworit@yandex.ru>
2019-06-24 23:54:21 +0300

    Fix

--- src/Firebase/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: 'AIzaSyAyyqLME_QyoliyURhsVjE_ykkxTOgNBoQ',
-  authDomain: 'ayazmemster.firebaseapp.com',
-  databaseURL: 'https://ayazmemster.firebaseio.com',
-  projectId: 'ayazmemster',
-  storageBucket: 'ayazmemster.appspot.com',
-  messagingSenderId: '748903039219',
-  appId: '1:748903039219:web:1646329bf7b4631c',
-};
