ab43121304a2fda43e46f22442a9df0d57ffeb83
Andrey Kiselev <omega-faworit@yandex.ru>
2020-11-21 14:18:25 +0300

    fix: add firebase modules import

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,5 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/analytics';
+import 'firebase/performance';
 
 const config = {
   apiKey: 'AIzaSyAyyqLME_QyoliyURhsVjE_ykkxTOgNBoQ',
