170490174cd4fc26b4af7db961cc87cf275c4e4b
Andrey Kiselev <omega-faworit@yandex.ru>
2020-11-22 12:43:59 +0300

    fix: card title is visible + add keyboard support

--- src/Homepage/Card.tsx
+++ src/Homepage/Card.tsx
@@ -1,6 +1,6 @@
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
-import React from 'react';
+import React, { KeyboardEvent, useCallback } from 'react';
 import { LazyLoadImage, ScrollPosition } from 'react-lazy-load-image-component';
 import { FlexboxGrid } from 'rsuite';
 
@@ -14,6 +14,11 @@ interface CardProps {
 }
 
 const Card = ({ meme, selected, scrollPosition }: CardProps) => {
+  const onEnterHandler = useCallback((e: KeyboardEvent<HTMLElement>) => {
+    console.log(e);
+    if (e.key === 'Enter') toggleSelected(meme);
+  }, []);
+
   return (
     <FlexboxGrid.Item
       style={{
@@ -23,25 +28,28 @@ const Card = ({ meme, selected, scrollPosition }: CardProps) => {
         boxShadow: '0 0 10px rgba(0,0,0,0.5)',
         margin: '10px 0',
         flexBasis: 350,
+        padding: 20,
+        paddingBottom: 5,
       }}
     >
-      <div
+      <article
         id={meme.title}
         style={{
           cursor: 'pointer',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
         }}
         onClick={() => {
           toggleSelected(meme);
         }}
+        onKeyPress={onEnterHandler}
+        tabIndex={0}
       >
         <LazyLoadImage
           src={meme.imageUrl}
           alt={meme.title}
           height={350}
-          style={{
-            margin: 20,
-            objectFit: 'contain',
-          }}
           scrollPosition={scrollPosition}
           effect="blur"
         />
@@ -50,7 +58,7 @@ const Card = ({ meme, selected, scrollPosition }: CardProps) => {
         >
           {meme.title}
         </figcaption>
-      </div>
+      </article>
     </FlexboxGrid.Item>
   );
 };
