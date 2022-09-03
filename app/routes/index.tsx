6c8ca51578e82d3f51acf2b88d0a4a74f7f1074f
Kiselev Andrey <omega-faworit@yandex.ru>
2022-09-04 02:04:42 +0300

    feat: add no memes message

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,8 @@
 import type { LoaderFunction } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
+import { Button } from '~/components/Button';
 import { MemeCard } from '~/components/MemeCard';
+import { useMemeSearch } from '~/components/Search';
 import { db } from '~/utils/db.server';
 import type { Meme } from '../types/meme';
 
@@ -35,19 +37,38 @@ export const loader: LoaderFunction = async ({
 export default function Index() {
   const { memes, totalMemes } = useLoaderData<LoaderData>();
 
+  const { search, clearSearch } = useMemeSearch();
+
   return (
-    <div>
+    <div className="h-full">
       <div className="flex justify-between items-center p-2">
         <h1 className="text-2xl font-semibold">Ayaz-Memster</h1>
         <p className="text-xl text-gray-500">
           {memes.length}/{totalMemes}
         </p>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-2 p-2 max-w-full">
-        {memes.map((meme) => {
-          return <MemeCard meme={meme} key={meme.id} />;
-        })}
-      </div>
+      {memes.length !== 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-2 p-2 max-w-full">
+          {memes.map((meme) => {
+            return <MemeCard meme={meme} key={meme.id} />;
+          })}
+        </div>
+      ) : (
+        <div className="flex flex-col h-full w-full justify-center items-center gap-2">
+          {search ? (
+            <>
+              <p className="text-lg">
+                No memes found for <strong>{search}</strong>
+              </p>
+              <Button onClick={clearSearch} className="">
+                Clear search
+              </Button>
+            </>
+          ) : (
+            <p className="text-lg">No memes 😞</p>
+          )}
+        </div>
+      )}
     </div>
   );
 }
