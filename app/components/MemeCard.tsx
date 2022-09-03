9c3f22a88e6f9b39de15a94d6601a503dcf96007
Kiselev Andrey <omega-faworit@yandex.ru>
2022-09-04 01:44:14 +0300

    feat: add lazy image loading

--- app/components/MemeCard.tsx
+++ app/components/MemeCard.tsx
@@ -14,7 +14,14 @@ export const MemeCard = (props: MemeCardProps) => {
       className="grid place-items-stretch p-3 shadow-md rounded-sm"
     >
       <figure className="flex flex-col">
-        <img src={`/api/images/${meme.id}/preview`} alt={meme.name} />
+        <img
+          src={`/api/images/${meme.id}/preview`}
+          alt={meme.name}
+          loading="lazy"
+          width="300"
+          height="300"
+          className="w-full"
+        />
         <figcaption className="text-lg font-semibold">{meme.name}</figcaption>
       </figure>
       <p className="text-gray-500 text-sm">Uploaded By: {meme.author}</p>
